2886645416bf3fdbeb61e58208cae1e16e66319a
Ahmad Elmasri <aelma090@uottawa.ca>
2019-09-19 16:18:45 -0400

    Initial commit

---
+++ app/src/main/java/com/example/seg2105walkinclinicservicesapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.seg2105walkinclinicservicesapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
